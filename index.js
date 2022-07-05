4563ab556c2360fc7729a64885110f48fe50f6f0
Joe Yu <joe.yu@tum.de>
2022-07-05 19:53:45 +0200

    - test sql statement

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ conn.connect(conn_params, function (err) {
 
     // Put listener for Photoelectric barrier here
     app.post('/increment', (req, res) => {
-        //time = new Date(dateStr).getTime()
-        conn.exec('INSERT INTO Raspdata VALUES (1, 24, 0, 1)',  function (err, result) {
+        //time = new Date().getTime()
+        conn.exec('INSERT INTO Raspdata VALUES (1, ?, 0, 1)', [124], function (err, result) {
             if (err) { res.send(err) };
             console.log("Increment:", result);
             res.send("Incremented counter")
@@ -31,7 +31,7 @@ conn.connect(conn_params, function (err) {
 
     app.post('/decrement', (req, res) => {
         //time = new Date(dateStr).getTime()
-        conn.exec('INSERT INTO Raspdata VALUES (1, 24, 0, -1)', function (err, result) {
+        conn.exec('INSERT INTO Raspdata VALUES (1, 24, 0, -1)', [124], function (err, result) {
             if (err) { res.send(err) };
             console.log("Decrement:", result);
             res.send("Decremented counter")
