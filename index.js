38ed689e037e76054b906f2315900af0ce0e3ac0
Joe Yu <joe.yu@tum.de>
2022-07-31 18:42:39 +0200

    - add comments for code collection

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//loading necessary modules
 const express = require('express')
 const cors = require("cors")
 const app = express()
@@ -5,8 +6,12 @@ const request = require('request');
 
 var hana = require('@sap/hana-client');
 
+//connect Node.js to SAP HANA Service Instance
 var conn = hana.createConnection();
 
+//Use the following connection string (parameters) to connect
+//serverNode: SQL Endpoint of SAP HANA Database Instance
+//uid and pwd of SAP HANA Database Instance (credentials)
 var conn_params = {
     serverNode: 'aa3ff0d9-4dbd-41b8-8568-fec280ef54d9.hana.trial-us10.hanacloud.ondemand.com:443',
     encrypt: 'true',
@@ -25,7 +30,6 @@ conn.connect(conn_params, function (err) {
         next();
     });
 
-    // MENSA DATA
     app.get('/meal-of-week', (req, res) => {
         const { year, kw } = req.query;
         request(
@@ -53,7 +57,8 @@ conn.connect(conn_params, function (err) {
         )
     });
 
-    // Put listener for Photoelectric barrier here
+    //endpoint to increase the counter by 1 --> adding a new row into the database with updated counter
+    //whenever someone crosses the photelectric barrier into the positive direction (going inside), this endpoint is called by the raspberry pi
     app.post('/increment', (req, res) => {
         conn.exec('INSERT INTO RASPDATA (counter) SELECT counter + ? FROM RASPDATA c1 WHERE c1.ts = (SELECT MAX(ts) FROM RASPDATA)', [1], function (err, result) {
             if (err) { res.send(err) };
@@ -62,7 +67,8 @@ conn.connect(conn_params, function (err) {
         })
     })
 
-
+    //endpoint to decrease the counter by 1 --> adding a new row into the database with updated counter
+    //whenever someone crosses the photelectric barrier into the negative direction (going outside), this endpoint is called by the raspberry pi
     app.post('/decrement', (req, res) => {
         conn.exec('INSERT INTO RASPDATA (counter) SELECT counter + ? FROM RASPDATA c1 WHERE c1.ts = (SELECT MAX(ts) FROM RASPDATA)', [-1], function (err, result) {
             if (err) { res.send(err) };
@@ -71,7 +77,7 @@ conn.connect(conn_params, function (err) {
         })
     })
 
-    //reset
+    //endpoint to reset the counter to 0 
     app.post('/reset', (req, res) => {
         conn.exec('INSERT INTO RASPDATA (counter) VALUES(0)', function (err, result) {
             if (err) { res.send(err) };
@@ -80,6 +86,8 @@ conn.connect(conn_params, function (err) {
         })
     })
 
+    //endpoint to retrieve the newest counter entry of the database 
+    //used to display the live occupancy in the web app
     app.get('/', (req, res) => {
         conn.exec('SELECT * FROM RASPDATA c1 WHERE c1.ts = (SELECT MAX(ts) FROM RASPDATA)', function (err, result) {
             if (err) { res.send(err) }
@@ -87,6 +95,8 @@ conn.connect(conn_params, function (err) {
         })
     })
 
+    //endpoint to retrieve the occupancy development
+    //used to display the development of occupancies as a bar chart in the web app
     app.get('/occupancies', (req, res) => {
         conn.exec('SELECT EXTRACT(YEAR FROM TS) as Year, EXTRACT(MONTH FROM TS) as MONTH, EXTRACT(DAY FROM TS) as DAY, cast(EXTRACT(HOUR FROM TS)+2 as numeric(36)) as HOUR, cast(FLOOR(EXTRACT(MINUTE FROM TS)/15)*15 as numeric(36)) as MINUTESINTERVAL, MEDIAN(COUNTER) FROM RASPDATA GROUP BY EXTRACT(YEAR FROM TS), EXTRACT(MONTH FROM TS), EXTRACT(DAY FROM TS), EXTRACT(HOUR FROM TS), cast(FLOOR(EXTRACT(MINUTE FROM TS)/15)*15 as numeric(36)) ORDER BY YEAR ASC, MONTH ASC, DAY ASC, HOUR ASC, MINUTESINTERVAL ASC', function (err, result) {
             if (err) { res.send(err) }
@@ -94,7 +104,7 @@ conn.connect(conn_params, function (err) {
         })
     })
 
-
+    //listen and bind the connections on the specified host and port
     app.listen(process.env.PORT || 5000,
         () => console.log("Server is running..."));
 });
